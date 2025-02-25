b2c46eb841bb842cde6ddbbdade487f20b1eac09
DanyelleH <danyellehenriquez@gmail.com>
2025-02-25 12:07:00 -0500

    loading component

--- PathoMap-Frontend/src/pages/LoginPage.jsx
+++ PathoMap-Frontend/src/pages/LoginPage.jsx
@@ -3,33 +3,49 @@ import Form from '../components/LoginForm';
 import { Navigate } from 'react-router-dom';
 import { login } from '../api/authApi';
 import { useNavigate } from "react-router-dom";
+import CircularColor from '../components/LoadingComponent';
 export default function Login({formData, handleInputChange,handleToken }){
     const [responseMsg, setResponseMsg] = useState("")
     const [shouldRedirect, setShouldRedirect] = useState(false)
-    const navigate = useNavigate()
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const context = {username: formData.username, password: formData.password}
+    const [loading, setLoading]=useState(false)
 
-    const token = await login(context)
-    if(!token) {
-      setResponseMsg("Error logging in")
-    } else {
-      handleToken(token)
-      setShouldRedirect(true)
-    }
-  }
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      try {
+        const context = {username: formData.username, password: formData.password}
+        const token = await login(context)
+        if(!token) {
+          setResponseMsg("Error logging in")
+        } else {
+          handleToken(token)
+          setShouldRedirect(true)
+        }
+      } catch (error) {
+        setResponseMsg("An error occured during login.")
+        console.error("Login Error:", error )
+      } finally {
+        setLoading(false);
+      }
+  };
   if (shouldRedirect) {
-    return <Navigate to="/home"/>
-  } else {
+    return <Navigate to="/home" />;
+  }
     return (
-        <>
+      <>
+        {loading ? (
+          <CircularColor />
+        ) : (
+          <>
             <Form formType={"Login"} handleInputChange={handleInputChange} formData={formData}handleToken={handleToken} handleSubmit={handleSubmit} responseMsg={responseMsg}/>
              <p> New User? </p>
             <button onClick={()=> navigate("/signup")} >Create an Account</button>
         </>
-    )
+        )}
+      </>
+    );
   }
-  
-}
+
 
