17298632e114590feb99dc91b3f55c44db717e9d
DanyelleH <danyellehenriquez@gmail.com>
2025-03-02 14:30:13 -0500

    compose debug

--- protein_proj/run_compose.sh
+++ protein_proj/run_compose.sh
@@ -1,5 +1,7 @@
-docker compose up --build
+docker-compose up -d --build
 sleep 5
 
 docker exec protein_proj-api-1 python /src/manage.py makemigrations 
 docker exec protein_proj-api-1 python /src/manage.py migrate 
+
+docker run -p 8090:80 pathomap-react-server
