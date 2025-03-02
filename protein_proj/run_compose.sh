d471be65793e313f03d4c3349ecce82c76aa1f24
DanyelleH <danyellehenriquez@gmail.com>
2025-03-02 14:52:17 -0500

    docker compose

--- protein_proj/run_compose.sh
+++ protein_proj/run_compose.sh
@@ -3,5 +3,3 @@ sleep 5
 
 docker exec protein_proj-api-1 python /src/manage.py makemigrations 
 docker exec protein_proj-api-1 python /src/manage.py migrate 
-
-docker run -p 8090:80 pathomap-react-server
